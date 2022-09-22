7b4a7df1c1cb92f093f0e00b730ed167f0bfbd02
ssaech <101516958+ssaech@users.noreply.github.com>
2022-09-21 20:48:26 -0700

    Add files via upload

---
+++ chatroom/server/PocketMonsters/PocketMonstersAPI/PocketMonstersAPI/Services/IPaginationMessagesService.cs
@@ -0,0 +1,15 @@
+﻿using PocketMonstersAPI.Models;
+namespace PocketMonstersAPI.Services;
+
+public interface IPaginationMessagesService
+{
+    Task<IEnumerable<MessageList>> GetAllAsync(int pg);
+
+    Task<IEnumerable<Output>> GetPagination(int pg );
+
+    Task<IResult> CreateAsync(MessageList mess);
+
+    Task<IResult> DeleteMesage(MessageOutput mess);
+
+
+}
